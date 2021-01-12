db2d4dfbb7e299ec8363d8b2b41350e3c052e362
Talin Crow <crowtech7@gmail.com>
2021-01-12 15:05:55 -0600

    First commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace lesson1HandsOn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           bool isSunny = true;
+           var wear = "Wear sunglasses!";
+           var dontWear = "You don't need to wear sunglasses.";
+
+           if (isSunny)
+            {
+                Console.WriteLine(wear);
+            }
+            else
+            {
+                Console.WriteLine(dontWear);
+            }
+
+        }
+    }
+}
